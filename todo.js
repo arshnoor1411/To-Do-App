58edc407c517d57307e19fd604c41b170de7e601
arshnoor1411 <32287091+arshnoor1411@users.noreply.github.com>
2019-03-15 17:24:21 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("ul").on("click","li",function(){
+  	$(this).toggleClass("completed");
+    });
+	$("ul").on("click","span",function(event)
+	{
+		$(this).parent().fadeOut(500,function()
+		{$(this).remove();
+		});
+		event.stop();
+	});
+	$("input[type='text']").keypress(function(event)
+	{
+		if(event.which==13)
+		{
+			var todo=$(this).val();
+			$("ul").append("<li><span><i class='fa fa-trash'></i></span>"+ todo+ "</li>");
+		}
+	});
+	$(".fa-plus").click(function()
+	{
+		$("input[type='text']").fadeToggle();
+	});
+});
